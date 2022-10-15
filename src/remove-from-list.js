154d30313e2d21cad9dfa594e2d3b0b9da5afd68
Abdyl-Aziz <azizshihabov@mail.ru>
2022-10-15 18:28:54 +0300

    feat: add solution for common-character-count task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+	console.log(l, k);
+	return l.filter((item) => item !== k);
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList,
 };
