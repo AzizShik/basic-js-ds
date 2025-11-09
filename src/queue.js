40017390cc429e55633f397b4adb6cb29e16c5c9
Abdyl-Aziz <azizshihabov@mail.ru>
2025-11-09 22:12:07 +0300

    feat: add solution for Queue task

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,22 +13,55 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-  getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+	constructor() {
+		this.head = null;
+		this.tail = null;
+	}
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+	getUnderlyingList() {
+		// Remove line below and write your code here
+		// throw new NotImplementedError('Not implemented');
+		function convert(node) {
+			if (!node) return null;
+			return {
+				value: node.value,
+				next: convert(node.next),
+			};
+		}
 
-  dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+		return convert(this.head);
+	}
+
+	enqueue(value) {
+		// Remove line below and write your code here
+		// throw new NotImplementedError('Not implemented');
+		const newNode = new ListNode(value);
+
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+	}
+
+	dequeue() {
+		// Remove line below and write your code here
+		// throw new NotImplementedError('Not implemented');
+		if (!this.head) return undefined;
+
+		const value = this.head.value;
+		this.head = this.head.next;
+
+		if (!this.head) {
+			this.tail = null;
+		}
+
+		return value;
+	}
 }
 
 module.exports = {
-  Queue
+	Queue,
 };
